04ed3a148ee2df32b4f669fb32dd373125d5245a
hrishikeshdarshan <hrishikesh7165@gmail.com>
2025-02-12 18:20:58 -0500

    Created UseCases for OnboardingPreferencesViewModel

--- auth/data/src/main/java/com/hrishi/auth/di/AuthDataModule.kt
+++ auth/data/src/main/java/com/hrishi/auth/di/AuthDataModule.kt
@@ -1,14 +1,21 @@
 package com.hrishi.auth.di
 
+import com.hrishi.domain.usecase.FormatExampleUseCase
 import com.hrishi.domain.usecase.IsUsernameDuplicateUseCase
 import com.hrishi.domain.usecase.IsUsernameValidUseCase
 import com.hrishi.domain.usecase.LoginUseCases
 import com.hrishi.domain.usecase.OnboardingPreferenceUseCases
+import com.hrishi.domain.usecase.ValidateSelectedPreferences
 import org.koin.dsl.module
 
 val authDataModule = module {
+    // Login
     single { IsUsernameValidUseCase() }
     factory { IsUsernameDuplicateUseCase() }
     single { LoginUseCases(get(), get()) }
+
+    // Onboarding Preferences
+    factory { ValidateSelectedPreferences() }
+    factory { FormatExampleUseCase(get()) }
     single { OnboardingPreferenceUseCases(get(), get()) }
 }
