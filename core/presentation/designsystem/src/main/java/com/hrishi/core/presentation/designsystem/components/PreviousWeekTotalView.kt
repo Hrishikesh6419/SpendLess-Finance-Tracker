7400def84d0ca9fd7b43964a5063df011d1011ef
hrishikeshdarshan <hrishikesh7165@gmail.com>
2025-02-20 21:11:26 -0500

    Created Top Components for the Dashboard Screen

---
+++ core/presentation/designsystem/src/main/java/com/hrishi/core/presentation/designsystem/components/PreviousWeekTotalView.kt
@@ -0,0 +1,71 @@
+package com.hrishi.core.presentation.designsystem.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.hrishi.core.presentation.designsystem.SpendLessFinanceTrackerTheme
+import com.hrishi.core.presentation.designsystem.secondaryFixed
+
+@Composable
+fun PreviousWeekTotalView(
+    modifier: Modifier = Modifier,
+    amount: String,
+    description: String,
+    backgroundColor: Color = secondaryFixed
+) {
+    Column(
+        modifier = modifier
+            .height(72.dp)
+            .background(
+                color = backgroundColor,
+                shape = RoundedCornerShape(16.dp)
+            )
+            .padding(start = 12.dp)
+            .padding(end = 36.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(
+            text = amount,
+            style = MaterialTheme.typography.titleLarge
+        )
+
+        Text(
+            modifier = Modifier.padding(top = 2.dp),
+            text = description,
+            style = MaterialTheme.typography.bodySmall.copy(
+                color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.7f),
+                fontSize = 12.sp
+            )
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PreviewPreviousWeekTotalView() {
+    SpendLessFinanceTrackerTheme {
+        Surface(
+            color = MaterialTheme.colorScheme.background
+        ) {
+            PreviousWeekTotalView(
+                modifier = Modifier.padding(12.dp),
+                description = "Largest transaction",
+                amount = "-\$59.99",
+            )
+        }
+    }
+}
