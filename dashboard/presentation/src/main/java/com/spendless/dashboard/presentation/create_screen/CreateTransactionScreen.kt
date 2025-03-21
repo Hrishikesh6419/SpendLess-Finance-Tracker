792ede1d78312b25da3c2b43f727e481ee47ed8d
hrishikeshdarshan <hrishikesh7165@gmail.com>
2025-03-20 20:08:34 -0400

    Refactored CreateTransactionScreen

--- dashboard/presentation/src/main/java/com/spendless/dashboard/presentation/create_screen/CreateTransactionScreen.kt
+++ dashboard/presentation/src/main/java/com/spendless/dashboard/presentation/create_screen/CreateTransactionScreen.kt
@@ -3,6 +3,7 @@ package com.spendless.dashboard.presentation.create_screen
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -12,18 +13,14 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -45,6 +42,8 @@ import com.hrishi.presentation.ui.LocalAuthActionHandler
 import com.hrishi.presentation.ui.ObserveAsEvents
 import com.hrishi.presentation.ui.UpdateDialogStatusBarAppearance
 import com.hrishi.presentation.ui.utils.getFormattedTitle
+import com.spendless.dashboard.core.R
+import kotlinx.coroutines.flow.Flow
 import org.koin.androidx.compose.koinViewModel
 import java.math.BigDecimal
 import java.time.LocalDateTime
@@ -58,28 +57,20 @@ fun CreateTransactionScreenRoot(
 ) {
     val authActionHandler = LocalAuthActionHandler.current
     UpdateDialogStatusBarAppearance(isDarkStatusBarIcons = false)
-    val scope = rememberCoroutineScope()
-    val context = LocalContext.current
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
-    val snackBarHostState = remember { SnackbarHostState() }
-    val keyboardController = LocalSoftwareKeyboardController.current
 
-    ObserveAsEvents(viewModel.events) { event ->
-        when (event) {
-            CreateTransactionEvent.CloseBottomSheet -> onDismiss()
-        }
-    }
+    CreateTransactionEventHandler(
+        events = viewModel.events,
+        onDismiss = onDismiss
+    )
 
     CreateTransactionScreen(
         modifier = modifier,
         uiState = uiState,
-        snackbarHostState = snackBarHostState,
         onAction = { action ->
             when (action) {
                 CreateTransactionAction.OnCreateClicked -> {
-                    authActionHandler?.invoke {
-                        viewModel.onAction(action)
-                    }
+                    authActionHandler?.invoke { viewModel.onAction(action) }
                 }
 
                 else -> viewModel.onAction(action)
@@ -89,10 +80,21 @@ fun CreateTransactionScreenRoot(
 }
 
 @Composable
+private fun CreateTransactionEventHandler(
+    events: Flow<CreateTransactionEvent>,
+    onDismiss: () -> Unit
+) {
+    ObserveAsEvents(events) { event ->
+        when (event) {
+            CreateTransactionEvent.CloseBottomSheet -> onDismiss()
+        }
+    }
+}
+
+@Composable
 private fun CreateTransactionScreen(
     modifier: Modifier = Modifier,
     uiState: CreateTransactionViewState,
-    snackbarHostState: SnackbarHostState,
     onAction: (CreateTransactionAction) -> Unit
 ) {
     Column(
@@ -100,138 +102,141 @@ private fun CreateTransactionScreen(
             .fillMaxSize()
             .padding(horizontal = 16.dp)
     ) {
-
         Spacer(modifier = Modifier.height(22.dp))
 
-        Row(
-            modifier = Modifier
-                .height(IntrinsicSize.Max)
-                .fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Text(
-                text = "Create Transaction",
-                style = MaterialTheme.typography.titleLarge
-            )
-
-            Icon(
-                modifier = Modifier.clickable {
-                    onAction(CreateTransactionAction.OnBottomSheetCloseClicked)
-                },
-                tint = Color.Unspecified,
-                imageVector = CloseIcon,
-                contentDescription = ""
-            )
-        }
+        CreateTransactionHeader(onAction)
 
         Spacer(modifier = Modifier.height(28.dp))
 
-        SegmentedSelector(
-            options = TransactionTypeUI.entries.toTypedArray(),
-            selectedOption = uiState.transactionType,
-            onOptionSelected = {
-                onAction(CreateTransactionAction.OnTransactionTypeChanged(it))
-            },
-            displayText = {
-                it.displayText()
-            },
-            displayIcon = { it.iconRes },
-            textSelectedColor = MaterialTheme.colorScheme.primary
-        )
+        TransactionTypeSelector(uiState, onAction)
 
         Spacer(modifier = Modifier.height(34.dp))
 
-        BasicTransactionField(
-            modifier = Modifier
-                .fillMaxWidth()
-                .align(Alignment.CenterHorizontally),
-            value = uiState.transactionName,
-            hint = uiState.transactionNameHint,
-            onValueChange = {
-                onAction(CreateTransactionAction.OnTransactionNameUpdated(it))
-            }
-        )
-
-        Spacer(modifier = Modifier.height(24.dp))
-
-        TransactionTextField(
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            value = uiState.amount,
-            currencySymbol = uiState.currency?.symbol ?: Currency.USD.symbol,
-            expenseFormat = uiState.expenseFormat,
-            decimalSeparator = uiState.decimalSeparatorUI ?: DecimalSeparatorUI.DOT,
-            thousandSeparator = uiState.thousandsSeparatorUI ?: ThousandsSeparatorUI.COMMA,
-            onValueChange = {
-                onAction(CreateTransactionAction.OnAmountUpdated(it))
-            },
-            isExpense = uiState.transactionType == TransactionTypeUI.EXPENSE
-        )
-
-        Spacer(modifier = Modifier.height(22.dp))
-
-        BasicTransactionField(
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            value = uiState.note,
-            hint = uiState.noteHint,
-            icon = PlusIcon,
-            emptyStateStyle = MaterialTheme.typography.bodyMedium.copy(
-                color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.60f)
-            ),
-            nonEmptyStateStyle = MaterialTheme.typography.bodyMedium.copy(
-                color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.60f)
-            ),
-            onValueChange = {
-                onAction(CreateTransactionAction.OnNoteUpdated(it))
-            }
-        )
-
-        Spacer(modifier = Modifier.height(32.dp))
-
-        if (uiState.showExpenseCategoryType) {
-            CategorySelector(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                showIconBackground = true,
-                fontStyle = MaterialTheme.typography.labelMedium,
-                selectedOption = uiState.transactionCategoryType,
-                options = TransactionCategoryTypeUI.expenseCategories(),
-                currencyDisplay = { it.symbol },
-                currencyTitleDisplay = { it.title },
-                onItemSelected = {
-                    onAction(CreateTransactionAction.OnTransactionCategoryUpdated(it))
-                }
-            )
-        }
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        CategorySelector(
-            modifier = Modifier
-                .fillMaxWidth(),
-            showIconBackground = true,
-            iconBackgroundColor = MaterialTheme.colorScheme.tertiaryContainer,
-            fontStyle = MaterialTheme.typography.labelMedium,
-            selectedOption = uiState.recurringType,
-            showMenuIcon = false,
-            options = RecurringTypeUI.entries.toTypedArray(),
-            currencyDisplay = { it.symbol },
-            currencyTitleDisplay = { it.getFormattedTitle(uiState.currentTime) },
-            onItemSelected = {
-                onAction(CreateTransactionAction.OnFrequencyUpdated(it))
-            }
-        )
+        TransactionFields(uiState, onAction)
 
         Spacer(modifier = Modifier.height(12.dp))
 
         SpendLessButton(
-            buttonText = "Create",
-            onClick = {
-                onAction(CreateTransactionAction.OnCreateClicked)
-            },
+            buttonText = stringResource(R.string.common_create_text),
+            onClick = { onAction(CreateTransactionAction.OnCreateClicked) },
             isEnabled = uiState.isCreateButtonEnabled
         )
     }
+}
+
+@Composable
+private fun CreateTransactionHeader(
+    onAction: (CreateTransactionAction) -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .height(IntrinsicSize.Max)
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            text = stringResource(R.string.create_transaction),
+            style = MaterialTheme.typography.titleLarge
+        )
+        Icon(
+            modifier = Modifier.clickable {
+                onAction(CreateTransactionAction.OnBottomSheetCloseClicked)
+            },
+            tint = Color.Unspecified,
+            imageVector = CloseIcon,
+            contentDescription = ""
+        )
+    }
+}
+
+@Composable
+private fun TransactionTypeSelector(
+    uiState: CreateTransactionViewState,
+    onAction: (CreateTransactionAction) -> Unit
+) {
+    SegmentedSelector(
+        options = TransactionTypeUI.entries.toTypedArray(),
+        selectedOption = uiState.transactionType,
+        onOptionSelected = { onAction(CreateTransactionAction.OnTransactionTypeChanged(it)) },
+        displayText = { it.displayText() },
+        displayIcon = { it.iconRes },
+        textSelectedColor = MaterialTheme.colorScheme.primary
+    )
+}
+
+@Composable
+private fun ColumnScope.TransactionFields(
+    uiState: CreateTransactionViewState,
+    onAction: (CreateTransactionAction) -> Unit
+) {
+    BasicTransactionField(
+        modifier = Modifier
+            .fillMaxWidth()
+            .align(Alignment.CenterHorizontally),
+        value = uiState.transactionName,
+        hint = uiState.transactionNameHint,
+        onValueChange = { onAction(CreateTransactionAction.OnTransactionNameUpdated(it)) }
+    )
+
+    Spacer(modifier = Modifier.height(24.dp))
+
+    TransactionTextField(
+        modifier = Modifier.align(Alignment.CenterHorizontally),
+        value = uiState.amount,
+        currencySymbol = uiState.currency?.symbol ?: Currency.USD.symbol,
+        expenseFormat = uiState.expenseFormat,
+        decimalSeparator = uiState.decimalSeparatorUI ?: DecimalSeparatorUI.DOT,
+        thousandSeparator = uiState.thousandsSeparatorUI ?: ThousandsSeparatorUI.COMMA,
+        onValueChange = { onAction(CreateTransactionAction.OnAmountUpdated(it)) },
+        isExpense = uiState.transactionType == TransactionTypeUI.EXPENSE
+    )
+
+    Spacer(modifier = Modifier.height(22.dp))
+
+    BasicTransactionField(
+        modifier = Modifier.align(Alignment.CenterHorizontally),
+        value = uiState.note,
+        hint = uiState.noteHint,
+        icon = PlusIcon,
+        emptyStateStyle = MaterialTheme.typography.bodyMedium.copy(
+            color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.60f)
+        ),
+        nonEmptyStateStyle = MaterialTheme.typography.bodyMedium.copy(
+            color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.60f)
+        ),
+        onValueChange = { onAction(CreateTransactionAction.OnNoteUpdated(it)) }
+    )
+
+    Spacer(modifier = Modifier.height(32.dp))
+
+    if (uiState.showExpenseCategoryType) {
+        CategorySelector(
+            modifier = Modifier.fillMaxWidth(),
+            showIconBackground = true,
+            fontStyle = MaterialTheme.typography.labelMedium,
+            selectedOption = uiState.transactionCategoryType,
+            options = TransactionCategoryTypeUI.expenseCategories(),
+            currencyDisplay = { it.symbol },
+            currencyTitleDisplay = { it.title },
+            onItemSelected = { onAction(CreateTransactionAction.OnTransactionCategoryUpdated(it)) }
+        )
+    }
+
+    Spacer(modifier = Modifier.height(8.dp))
+
+    CategorySelector(
+        modifier = Modifier.fillMaxWidth(),
+        showIconBackground = true,
+        iconBackgroundColor = MaterialTheme.colorScheme.tertiaryContainer,
+        fontStyle = MaterialTheme.typography.labelMedium,
+        selectedOption = uiState.recurringType,
+        showMenuIcon = false,
+        options = RecurringTypeUI.entries.toTypedArray(),
+        currencyDisplay = { it.symbol },
+        currencyTitleDisplay = { it.getFormattedTitle(uiState.currentTime) },
+        onItemSelected = { onAction(CreateTransactionAction.OnFrequencyUpdated(it)) }
+    )
 }
 
 @Preview
@@ -255,10 +260,7 @@ private fun PreviewCreateTransactionScreenRoot() {
                     isCreateButtonEnabled = false,
                     currentTime = LocalDateTime.of(2025, Month.JANUARY, 15, 10, 0)
                 ),
-                snackbarHostState = SnackbarHostState(),
-                onAction = {
-
-                }
+                onAction = { }
             )
         }
     }
