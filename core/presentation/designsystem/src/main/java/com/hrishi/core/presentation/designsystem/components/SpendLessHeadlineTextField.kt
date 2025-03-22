6fc9223b54909ddd1dccedfa71095fd5d2ec7eb3
hrishikeshdarshan <hrishikesh7165@gmail.com>
2025-03-22 00:10:48 -0400

    Refactored SpendLessHeadlineTextField

--- core/presentation/designsystem/src/main/java/com/hrishi/core/presentation/designsystem/components/SpendLessHeadlineTextField.kt
+++ core/presentation/designsystem/src/main/java/com/hrishi/core/presentation/designsystem/components/SpendLessHeadlineTextField.kt
@@ -3,7 +3,7 @@ package com.hrishi.core.presentation.designsystem.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -12,7 +12,6 @@ import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -23,14 +22,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.intl.Locale
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.toLowerCase
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.hrishi.core.presentation.designsystem.SpendLessFinanceTrackerTheme
+import java.util.Locale
 
 @Composable
 fun SpendLessHeadlineTextField(
@@ -39,81 +37,111 @@ fun SpendLessHeadlineTextField(
     hint: String,
     keyboardType: KeyboardType = KeyboardType.Text,
     imeAction: ImeAction = ImeAction.Next,
+    enabled: Boolean = true,
     modifier: Modifier = Modifier,
     onDone: (() -> Unit)? = null
 ) {
     var isFocused by remember { mutableStateOf(false) }
 
-    BasicTextField(
-        value = value,
-        maxLines = 1,
-        onValueChange = onValueChange,
-        textStyle = MaterialTheme.typography.displayMedium.copy(
-            textAlign = TextAlign.Center
-        ),
-        keyboardOptions = KeyboardOptions(
-            keyboardType = keyboardType,
-            imeAction = imeAction
-        ),
-        keyboardActions = KeyboardActions(
-            onDone = {
-                onDone?.invoke()
-            }
-        ),
+    Box(
         modifier = modifier
             .fillMaxWidth()
-            .onFocusChanged { isFocused = it.isFocused }
             .defaultMinSize(minHeight = 64.dp)
             .clip(RoundedCornerShape(16.dp))
             .background(
-                MaterialTheme.colorScheme.onBackground.copy(
-                    alpha = 0.08f
+                color = MaterialTheme.colorScheme.onBackground.copy(
+                    alpha = if (enabled) 0.08f else 0.04f
                 )
             ),
-        decorationBox = { innerBox ->
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.Center,
-                modifier = Modifier.padding(
-                    start = 16.dp,
-                    end = 16.dp,
-                    top = 10.dp,
-                    bottom = 10.dp
-                )
-            ) {
-                Box(modifier = Modifier.weight(1f)) {
-                    // TODO: Added isFocused to hide hint when value is empty,
-                    //  if not cursor was being shown in between hint.
-                    //  Maybe take a look later?
-                    if (value.isEmpty() && !isFocused) {
-                        Text(
-                            modifier = Modifier.fillMaxWidth(),
-                            text = hint.toLowerCase(Locale.current),
-                            style = MaterialTheme.typography.displayMedium,
-                            color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.38f),
-                            textAlign = TextAlign.Center
-                        )
-                    }
-                    innerBox()
+        contentAlignment = Alignment.Center
+    ) {
+        BasicTextField(
+            value = value,
+            onValueChange = onValueChange,
+            enabled = enabled,
+            maxLines = 1,
+            textStyle = MaterialTheme.typography.displayMedium.copy(
+                textAlign = TextAlign.Center
+            ),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = keyboardType,
+                imeAction = imeAction
+            ),
+            keyboardActions = KeyboardActions(
+                onDone = {
+                    onDone?.invoke()
                 }
+            ),
+            cursorBrush = SolidColor(MaterialTheme.colorScheme.primary),
+            modifier = Modifier
+                .fillMaxWidth()
+                .onFocusChanged { isFocused = it.isFocused },
+            decorationBox = { innerBox ->
+                HeadlineTextFieldContent(
+                    value = value,
+                    hint = hint,
+                    isFocused = isFocused,
+                    enabled = enabled,
+                    innerTextField = innerBox
+                )
             }
-        }
-    )
+        )
+    }
 }
 
-@Preview
 @Composable
-fun PreviewSpendLessHeadlineTextField() {
-    SpendLessFinanceTrackerTheme {
-        Surface(
-            color = MaterialTheme.colorScheme.background
-        ) {
-            SpendLessHeadlineTextField(
-                value = "",
-                onValueChange = {},
-                hint = "Username",
-                modifier = Modifier.padding(16.dp),
+private fun HeadlineTextFieldContent(
+    value: String,
+    hint: String,
+    isFocused: Boolean,
+    enabled: Boolean,
+    innerTextField: @Composable () -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 10.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        if (value.isEmpty() && !isFocused) {
+            Text(
+                text = hint.lowercase(Locale.getDefault()),
+                style = MaterialTheme.typography.displayMedium,
+                color = MaterialTheme.colorScheme.onSurface.copy(
+                    alpha = if (enabled) 0.38f else 0.25f
+                ),
+                textAlign = TextAlign.Center
             )
         }
+
+        Box(
+            modifier = Modifier.fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        ) {
+            innerTextField()
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewSpendLessHeadlineTextField() {
+    Column(
+        modifier = Modifier.padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        SpendLessHeadlineTextField(
+            value = "",
+            onValueChange = {},
+            hint = "Username",
+            modifier = Modifier.fillMaxWidth(),
+        )
+
+        SpendLessHeadlineTextField(
+            value = "James Bond",
+            onValueChange = {},
+            hint = "Username",
+            modifier = Modifier.fillMaxWidth(),
+        )
     }
 }
